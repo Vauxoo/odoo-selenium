9cb6f7610f9b592457e702f42140a8505a9d1ca1
antonag32 <antonio@vauxoo.com>
2023-08-24 17:09:29 -0600

    Refactor mixin: inherit directly from HttpCase
    In order to reduce boilerplate needed to boostrap Selenium,
    SeleniumMixin was converted into a test case by being a subclass of
    HttpCase.
    
    Its navigate method will not require a full URL either, behaving in
    similar fashion as Odoo's `start_tour`, which only takes a
    relative (to Odoo's base) URL.

--- src/odoo_selenium/__init__.py
+++ src/odoo_selenium/__init__.py
@@ -1,4 +1,4 @@
 """Utilities that make it easy to quickly write and develop Odoo tests using Selenium."""
 __version__ = "0.1.2"
 
-from odoo_selenium.selenium import SeleniumMixin
+from odoo_selenium.selenium import SeleniumCase
--- tests_odoo/test_selenium/tests/test_selenium.py
+++ tests_odoo/test_selenium/tests/test_selenium.py
@@ -1,26 +1,15 @@
 from selenium.webdriver.common.by import By
 
-from odoo.tests import HttpCase, tagged
+from odoo.tests import tagged
 
-from odoo_selenium import SeleniumMixin
+from odoo_selenium import SeleniumCase
 
 
 @tagged("-at_install", "post_install")
-class TestSelenium(SeleniumMixin, HttpCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-    def setUp(self):
-        super().setUp()
-        self.start_selenium()
-
-    def tearDown(self):
-        super().tearDown()
-        self.stop_selenium()
+class TestSelenium(SeleniumCase):
 
     def test_login_page(self):
-        self.navigate(f"{self.odoo_url}/web/login")
+        self.navigate("/web/login")
         email_input = self.driver.find_element(By.ID, "login")
 
         self.assertEqual("Email", email_input.get_attribute("placeholder"))

e0a5ebc98f1ef53b8fc77c24b40e59a019b81f5a
Moisés López <moylop260@vauxoo.com>
2023-08-24 23:28:17 -0600

    Bump version: 0.1.1 → 0.1.2

--- src/odoo_selenium/__init__.py
+++ src/odoo_selenium/__init__.py
@@ -1,4 +1,4 @@
 """Utilities that make it easy to quickly write and develop Odoo tests using Selenium."""
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from odoo_selenium.selenium import SeleniumMixin

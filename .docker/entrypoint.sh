9dcfd5d0c318f783a5863faaeb91e51ddb623ab4
antonag32 <antonio@vauxoo.com>
2024-08-13 17:01:25 -0600

    [CI] actually fail job when tests fail
    The exit code from tests was being masked by the exit codes reported
    when generating coverage reports which meant that even if tests failed,
    as long as coverage report generation was succesful, the job would not
    fail.
    
    Therefore the exit code for tests is now stored and used.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 coverage run /usr/bin/odoo -i test_selenium --test-tags /test_selenium --workers=0 --stop-after-init
+TEST_EXIT_CODE=$?
+
 coverage report --show-missing
 coverage html
 coverage xml
+
+exit "$TEST_EXIT_CODE"

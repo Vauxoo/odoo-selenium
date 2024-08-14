65e68f841ca7717f3be43de38338e1f40694bebd
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2024-08-13 18:02:11 -0600

    Bump version: 0.1.5 → 0.1.6

--- src/odoo_selenium/__init__.py
+++ src/odoo_selenium/__init__.py
@@ -1,4 +1,4 @@
 """Utilities that make it easy to quickly write and develop Odoo tests using Selenium."""
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from odoo_selenium.selenium import SeleniumCase
